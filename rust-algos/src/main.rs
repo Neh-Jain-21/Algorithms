44c82f3b851b558977b424c9fc72d68cd06a9133
nehjain.2001 <nehjain.2001@gmail.com>
2024-12-10 12:27:13 +0000

    MEREG SORT

--- rust-algos/src/main.rs
+++ rust-algos/src/main.rs
@@ -3,7 +3,7 @@ use rust_algos::sorting::merge_sort::{merge_sort, MAX_MERGE_SORT_SIZE};
 fn main() {
     let mut data: [i32; MAX_MERGE_SORT_SIZE] = [0, 10, 40, 30];
 
-    merge_sort(&mut data);
+    merge_sort(&mut data, None, None);
 
     println!("{:?}", data);
 }
--- rust-algos/src/sorting/merge_sort.rs
+++ rust-algos/src/sorting/merge_sort.rs
@@ -1,19 +1,51 @@
 pub const MAX_MERGE_SORT_SIZE: usize = 4;
 
-fn divide_and_conquer(data: &mut [i32; MAX_MERGE_SORT_SIZE], lower_bound: i32, upper_bound: i32) {
-    let sliced_data: &mut [i32] = &mut data[lower_bound as usize..(upper_bound + 1) as usize];
+fn sort(data: &mut [i32; MAX_MERGE_SORT_SIZE], lower_bound: i32, upper_bound: i32, mid: i32) {
+    let mut i: i32 = lower_bound;
+    let mut j: i32 = mid + 1;
+    let mut k: i32 = lower_bound;
+    let mut temp: [i32; MAX_MERGE_SORT_SIZE] = [0, 0, 0, 0];
 
-    if sliced_data.len() == 2 && sliced_data[0] > sliced_data[1] {
-        sliced_data.swap(0, 1);
+    while i <= mid && j <= upper_bound {
+        if data[i as usize] < data[j as usize] {
+            temp[k as usize] = data[i as usize];
+            i += 1;
+        } else {
+            temp[k as usize] = data[j as usize];
+            j += 1;
+        }
+
+        k += 1;
+    }
+
+    while i <= mid {
+        temp[k as usize] = data[i as usize];
+        i += 1;
+        k += 1;
+    }
+
+    while j <= upper_bound {
+        temp[k as usize] = data[j as usize];
+        j += 1;
+        k += 1;
+    }
+
+    for i in lower_bound..upper_bound + 1 {
+        data[i as usize] = temp[i as usize];
     }
 }
 
-pub fn merge_sort(data: &mut [i32; MAX_MERGE_SORT_SIZE]) {
+pub fn merge_sort(data: &mut [i32; MAX_MERGE_SORT_SIZE], lb: Option<i32>, up: Option<i32>) {
     let total_length: i32 = data.len().try_into().unwrap();
-    let mid: i32 = ((total_length - 1) / 2).try_into().unwrap();
 
-    divide_and_conquer(data, 0, mid);
-    divide_and_conquer(data, mid + 1, total_length - 1);
+    let lower_bound: i32 = lb.unwrap_or(0);
+    let upper_bound: i32 = up.unwrap_or(total_length);
 
-    println!("{:?}", data);
+    if lower_bound < upper_bound {
+        let mid: i32 = (lower_bound + upper_bound) / 2;
+
+        merge_sort(data, Some(lower_bound), Some(mid));
+        merge_sort(data, Some(mid + 1), Some(upper_bound));
+        sort(data, lower_bound, upper_bound - 1, mid);
+    }
 }
