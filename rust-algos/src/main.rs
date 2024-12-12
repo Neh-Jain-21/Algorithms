fbef8ced026d1b002aaeff70343621a9a7063b9e
nehjain.2001 <nehjain.2001@gmail.com>
2024-12-12 16:39:33 +0000

    MERGE SORT

--- rust-algos/src/main.rs
+++ rust-algos/src/main.rs
@@ -1,9 +1,9 @@
-use rust_algos::sorting::merge_sort::{merge_sort, MAX_MERGE_SORT_SIZE};
+use rust_algos::sorting::merge_sort::merge_sort;
 
 fn main() {
-    let mut data: [i32; MAX_MERGE_SORT_SIZE] = [12, 11, 13, 5, 6, 7];
+    let mut data: Vec<i32> = vec![12, 11, 13, 5, 6, 7];
 
-    let total_length: i32 = data.len().try_into().unwrap();
+    let total_length = data.len();
 
     merge_sort(&mut data, 0, total_length - 1);
 
--- rust-algos/src/sorting/merge_sort.rs
+++ rust-algos/src/sorting/merge_sort.rs
@@ -1,59 +1,52 @@
-pub const MAX_MERGE_SORT_SIZE: usize = 6;
+fn merge(arr: &mut Vec<i32>, left: usize, mid: usize, right: usize) {
+    let n1: usize = mid - left + 1;
+    let n2: usize = right - mid;
 
-fn merge(arr: &mut [i32; MAX_MERGE_SORT_SIZE], left: i32, mid: i32, right: i32) {
-    let n1: i32 = mid - left + 1;
-    let n2: i32 = right - mid;
+    let mut l: Vec<i32> = vec![0; n1];
+    let mut r: Vec<i32> = vec![0; n2];
 
-    let mut l: [i32; MAX_MERGE_SORT_SIZE] = [0, 0, 0, 0, 0, 0];
-    let mut r: [i32; MAX_MERGE_SORT_SIZE] = [0, 0, 0, 0, 0, 0];
-
-    for i in 0..(n1 - 1) {
-        l[i as usize] = arr[(left + i) as usize];
+    for i in 0..n1 {
+        l[i] = arr[left + i];
     }
 
-    for j in 0..(n2 - 1) {
-        r[j as usize] = arr[(mid + 1 + j) as usize];
+    for j in 0..n2 {
+        r[j] = arr[mid + 1 + j];
     }
 
-    let mut i: i32 = 0;
-    let mut j: i32 = 0;
-    let mut k: i32 = left;
+    let mut i: usize = 0;
+    let mut j: usize = 0;
+    let mut k: usize = left;
 
     while i < n1 && j < n2 {
-        if l[i as usize] <= r[j as usize] {
-            arr[k as usize] = l[i as usize];
+        if l[i] <= r[j] {
+            arr[k] = l[i];
             i += 1;
         } else {
-            arr[k as usize] = r[j as usize];
+            arr[k] = r[j];
             j += 1;
         }
-
         k += 1;
     }
 
     while i < n1 {
-        arr[k as usize] = l[i as usize];
+        arr[k] = l[i];
         i += 1;
         k += 1;
     }
 
     while j < n2 {
-        arr[k as usize] = r[j as usize];
+        arr[k] = r[j];
         j += 1;
         k += 1;
     }
 }
 
-pub fn merge_sort(arr: &mut [i32; MAX_MERGE_SORT_SIZE], left: i32, right: i32) {
-    if left >= right {
-        return;
+pub fn merge_sort(arr: &mut Vec<i32>, left: usize, right: usize) {
+    if left < right {
+        let mid: usize = (left + right) / 2;
+
+        merge_sort(arr, left, mid);
+        merge_sort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
-
-    let mid: i32 = (((left + (right - left)) / 2) as f32).floor() as i32;
-
-    merge_sort(arr, left, mid);
-    merge_sort(arr, mid + 1, right);
-    merge(arr, left, mid, right);
-
-    println!("{:?}", arr);
 }
